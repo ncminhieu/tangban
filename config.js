473894f6a720e63593748f071c9367324e0e2928
ncminhieu <148377966+ncminhieu@users.noreply.github.com>
2023-10-20 10:10:46 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'The Women';
+const giftUrl = 'https://images.app.goo.gl/orCY68tJ1YwktVB98';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng bạn';
+const contentCard = ' Nhân ngày Phụ nữ Việt Nam 20/10, xin chúc bạn sẽ luôn giữ được sự duyên dáng và xinh đẹp tuyệt vời trong mắt người đàn ông của mình nhé.';
+
+
+const giftImage = 'tng-hp-nhng-li-chuc-2010-hay-va-y-nghia-nht_c1dfd4dc.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
